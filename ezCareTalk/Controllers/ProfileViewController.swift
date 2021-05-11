85cb9cdbd3752490e9f7df942d2acb7a7c8810c9
leesoonbin <qklove@daum.net>
2021-05-11 09:06:29 +0900

    ProfileViewController

--- ezCareTalk/Controllers/ProfileViewController.swift
+++ ezCareTalk/Controllers/ProfileViewController.swift
@@ -7,16 +7,69 @@
 //
 
 import UIKit
+import FirebaseAuth
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                    return data.count
 
-class ProfileViewController: UIViewController {
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.textColor = .red
 
+        return cell
+    }
+    
+
+    @IBOutlet var tableView: UITableView!
+    
+    let data = ["Log Out"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
 
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+
+            let actionSheet = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+            actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { [weak self]_ in
+                
+                guard let strongSelf = self else {
+                    return
+                }
+                
+                do {
+                    try FirebaseAuth.Auth.auth().signOut()
+                    
+                    let vc = LoginViewController()
+                    let nav = UINavigationController(rootViewController: vc)
+                    nav.modalPresentationStyle = .fullScreen
+                        strongSelf.present(nav, animated: true)
+                }
+                catch {
+                    print("Failed to log out")
+                }
+            }))
+            
+            actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            
+            present(actionSheet, animated: true)
+            
+            
+            
+        }
+    }
     /*
     // MARK: - Navigation
 
@@ -27,4 +80,4 @@ class ProfileViewController: UIViewController {
     }
     */
 
-}
+
--- ezCareTalk/Resources/DatabaseManager.swift
+++ ezCareTalk/Resources/DatabaseManager.swift
@@ -19,7 +19,10 @@ final class DatabaseManager {
 extension DatabaseManager {
 
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of:  .value, with: { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        database.child(safeEmail).observeSingleEvent(of:  .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 
                 completion(false)
@@ -36,7 +39,7 @@ extension DatabaseManager {
     
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser){
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -49,5 +52,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
     //let profilePictureUrl: String
 }
